9d8471297720d8de05d24563664f34a051387ece
rebegul <rebeca_gul@outlook.es>
2021-10-07 22:56:45 -0500

    mod 4

---
+++ ProyectoIntegracion/Controladores/SesionTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SesionTableViewController.swift
+//  ProyectoIntegracion
+//
+//  Created by Rebeca Guevara on 07/10/21.
+//
+
+import UIKit
+
+class SesionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        super.viewDidLoad()
+        navigationItem.title = "Sesiones"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
